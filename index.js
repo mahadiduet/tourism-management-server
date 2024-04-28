f8f92afcdd43dff7ca0e6ad9fc3aff8a078cb399
mahadiduet <mahadiduet@gmail.com>
2024-04-28 21:55:48 +0600

    server site setup

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   res.send('Server side running...')
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mahadiduet:m9DxQxeQNdnHXlGo@cluster0.lyuai16.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      //  await client.connect();
+      const userCollection = client.db("tourismDB").collection("users");
+      const tourismCollection = client.db("tourismDB").collection("tourism");
+
+      //  User Create
+      app.post('/user', async (req, res) => {
+         const data = req.body;
+         const result = await userCollection.insertOne(data);
+         console.log(`A document was inserted with the _id: ${result}`);
+         res.send(result);
+      })
+
+      // View Tourism spot
+      app.get('/tourism-sport', async (req, res) => {
+         
+         const data = await tourismCollection.find().toArray();
+         // const data = cursor.toArray();
+         res.send(data);
+      })
+
+      // Add tourism spot
+      app.post('/add-tourism-sport', async (req, res) => {
+         const tourism = req.body;
+         console.log(tourism);
+         const result = await tourismCollection.insertOne(tourism);
+         console.log(`A document was inserted with the _id: ${result}`);
+         res.send(result);
+      })
+
+
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      //  await client.close();
+   }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+   console.log('Server site port run:', port);
+})
