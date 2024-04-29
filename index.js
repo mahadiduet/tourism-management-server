4e7be72bb8466556df3a4eca5928f04e6101bfc4
mahadiduet <mahadiduet@gmail.com>
2024-04-30 01:39:02 +0600

    api clean

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://mahadiduet:EJMEfxwMN9szBmNF@cluster0.lyuai16.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
    serverApi: {
       version: ServerApiVersion.v1,
@@ -24,12 +23,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      //  await client.connect();
       const userCollection = client.db("tourismDB").collection("users");
       const tourismCollection = client.db("tourismDB").collection("tourism");
 
-      //  User Create
+      //  User Create API
       app.post('/user', async (req, res) => {
          const data = req.body;
          const result = await userCollection.insertOne(data);
@@ -37,86 +34,79 @@ async function run() {
          res.send(result);
       })
 
-      // View Tourism spot
+      // View Tourism spot API for homepage section
       app.get('/tourism-sport', async (req, res) => {
-
          const data = await tourismCollection.find().sort({ _id: -1 }).limit(6).toArray();
-         // const data = cursor.toArray();
          res.send(data);
       })
 
-      // All tourism spot
+      // All tourism spot API
       app.get('/all-tourism-sport', async (req, res) => {
-
          const data = await tourismCollection.find().sort({ _id: -1 }).toArray();
-         // const data = cursor.toArray();
          res.send(data);
       })
 
-      // My Added spot 
+      // My tourism spot API
       app.get('/my-tourism-sport', async (req, res) => {
          const email = req.query.email;
          const data = await tourismCollection.find({ 'user_email': email }).sort({ _id: -1 }).toArray();
          res.send(data);
       })
 
-      // View details spot 
+      // View details spot page API
       app.get('/view-details', async (req, res) => {
          const id = new ObjectId(req.query.id);
-         console.log(id);
          const data = await tourismCollection.find({ '_id': id }).toArray();
-         console.log(data)
          res.send(data);
       })
 
-      // Update Spot 
+      // Update Tourism Spot API
       app.get('/update', async (req, res) => {
          const id = new ObjectId(req.query.id);
-         console.log(id);
          const data = await tourismCollection.find({ '_id': id }).toArray();
-         console.log(data)
          res.send(data);
       })
 
-      // Delete tourism
+      // Delete tourism spot API
       app.delete('/my-tourism-sport/:id', async (req, res) => {
          const tourism_id = new ObjectId(req.params.id);
-         // console.log(tourism_id);
-         const query = {_id: tourism_id};
+         const query = { _id: tourism_id };
          const result = await tourismCollection.deleteOne(query);
          res.send(result);
       });
 
-      // Add tourism spot
+      // Add tourism spot API
       app.post('/add-tourism-sport', async (req, res) => {
          const tourism = req.body;
-         console.log(tourism);
          const result = await tourismCollection.insertOne(tourism);
          console.log(`A document was inserted with the _id: ${result}`);
          res.send(result);
       })
 
-      // Single tourism get
-      app.get('/all-tourism-sport/:id', async(req, res)=>{
+      // Single tourism spot API
+      app.get('/all-tourism-sport/:id', async (req, res) => {
          const id = req.params.id;
          const query = new ObjectId(id);
          const result = await tourismCollection.findOne(query);
          res.send(result);
       })
 
-      // Update tourism spot
+      // Country tourism spot API
+      app.get('/country/:id', async (req, res) => {
+         const country = req.params.id;
+         const data = await tourismCollection.find({ 'country_Name': country }).sort({ _id: -1 }).toArray();
+         res.send(data);
+      })
+
+      // Update tourism spot API
       app.put('/update-tourism-spot/:id', async (req, res) => {
          const id = req.params.id;
-         // console.log(id);
          const filter = { _id: new ObjectId(id) };
          const options = { upsert: true };
          const updateData = {
             $set: req.body
-          };
-         // const tourism = req.body;
-         // console.log(tourism);
+         };
          const result = await tourismCollection.updateOne(filter, updateData, options);
-         // const result = await tourismCollection.insertOne(tourism);
          console.log(`A document was inserted with the _id: ${result}`);
          res.send(result);
       })
